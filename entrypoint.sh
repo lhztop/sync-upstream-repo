4aae04f2dc67a0a36aa52c3cd3eceac17a9b16b3
lihz <lhztop@gmail.com>
2024-04-19 14:04:48 +0800

    upstream by curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,7 @@ set -x
 
 UPSTREAM_REPO=$1
 UPSTREAM_BRANCH=$2
-DOWNSTREAM_BRANCH=$3
-GITHUB_TOKEN=$4
-FETCH_ARGS=$5
-MERGE_ARGS=$6
-PUSH_ARGS=$7
-SPAWN_LOGS=$8
+GITHUB_TOKEN=$3
 
 if [[ -z "$UPSTREAM_REPO" ]]; then
   echo "Missing \$UPSTREAM_REPO"
@@ -22,49 +17,19 @@ if [[ -z "$DOWNSTREAM_BRANCH" ]]; then
   DOWNSTREAM_BREANCH=UPSTREAM_BRANCH
 fi
 
-if ! echo "$UPSTREAM_REPO" | grep '\.git'; then
-  UPSTREAM_REPO="https://github.com/${UPSTREAM_REPO_PATH}.git"
-fi
-
 echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 
-git clone "https://github.com/${GITHUB_REPOSITORY}.git" work
-cd work || { echo "Missing work dir" && exit 2 ; }
+declare -a array=($UPSTREAM_REPO)
 
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git config --local user.password ${GITHUB_TOKEN}
-
-git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-git remote add upstream "$UPSTREAM_REPO"
-git fetch ${FETCH_ARGS} upstream
-git remote -v
-
-git checkout ${DOWNSTREAM_BRANCH}
-
-case ${SPAWN_LOGS} in
-  (true)    echo -n "sync-upstream-repo https://github.com/dabreadman/sync-upstream-repo keeping CI alive."\
-            "UNIX Time: " >> sync-upstream-repo
-            date +"%s" >> sync-upstream-repo
-            git add sync-upstream-repo
-            git commit sync-upstream-repo -m "Syncing upstream";;
-  (false)   echo "Not spawning time logs"
-esac
-
-git push origin
-
-MERGE_RESULT=$(git merge ${MERGE_ARGS} upstream/${UPSTREAM_BRANCH})
+for repo in "${array[@]}"
+do
+  echo $repo
+  curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/merge-upstream \
+  -d '{"branch":"main"}'
+done
 
 
-if [[ $MERGE_RESULT == "" ]] 
-then
-  exit 1
-elif [[ $MERGE_RESULT != *"Already up to date."* ]]
-then
-  git commit -m "Merged upstream"
-  git push ${PUSH_ARGS} origin ${DOWNSTREAM_BRANCH} || exit $?
-fi
-
-cd ..
-rm -rf work
